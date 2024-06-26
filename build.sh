a28e7d7c5a717c318e1153ac0e7b6e7c16ffde11
gpaolino <gp.guglielminopaolo@me.com>
2024-06-26 22:02:48 +0200

    Fix path in build script

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ python3 -m venv dist/venv
 source dist/venv/bin/activate
 python3 -m pip install -r requirements.txt
 python3 -m pip install -r build_requirements.txt
-shiv --site-packages venv/lib/python3.10/site-packages \
+shiv --site-packages dist/venv/lib/python3.10/site-packages \
         --compressed \
         -o caffeine \
         -e caffeine.__main__:main src/ \
